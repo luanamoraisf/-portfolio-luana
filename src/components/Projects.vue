5670499bc2deb8513e7f6c26cb8eda54be610d57
luanamoraisf <luanaflorenciopc@gmail.com>
2024-06-21 15:08:27 -0300

    adjustments and add title of the first project

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,13 +2,14 @@
     <h1 class="title">Projetos</h1>
   <div class="card1"></div>
 
-  <div class="but1">
-    <h2 class="technology1">Tailwind</h2>
-  </div>
-  <div class="but2">
-    <h2 class="technology2">Laravel</h2>
+  <div class="but1"></div>
+  <div class="but2"></div>
 
-  </div>
+  <h2 class="technology1">Tailwind</h2>
+  <h2 class="technology2">Laravel</h2>
+
+  <h2 class="project1">econômico</h2>
+  <!-- <h2>O projeto nomeado Ecônomico foi um sistema desenvolvido com o intuito de auxiliar jovens e adultos no controle financeiro.</h2> -->
 </template>
 
 <script>
@@ -30,7 +31,7 @@ export default {
     color: #EFEFF0;
     font-family: serif;
     margin-bottom: 40%;
-    margin-top: -40%;
+    margin-top: -42%;
 }
 .but1{
     border-right: 70px solid #be61c0;
@@ -55,9 +56,23 @@ export default {
 .technology1{
     color: #EFEFF0;
     font-size: 20px;
+    margin-bottom: 50%;
+    margin-top: -52.7%;
+    margin-right: 30.5%;
 }
 .technology2{
     color: #EFEFF0;
     font-size: 20px;
+    margin-bottom: 50%;
+    margin-top: -52.5%;
+    margin-right: 54%;
+}
+.project1{
+    color: #1A1D22;
+    font-size: 30px;
+    margin-bottom: 50%;
+    margin-top: -48%;
+    margin-right: 82%;
+    font-weight: bold;
 }
 </style>
