74f1d7e8aa646605059a47b9b7f928e7abdfb3cd
luanamoraisf <luanaflorenciopc@gmail.com>
2024-06-20 22:12:50 -0300

    adding style and info about technology

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,6 +1,14 @@
 <template>
     <h1 class="title">Projetos</h1>
   <div class="card1"></div>
+
+  <div class="but1">
+    <h2 class="technology1">Tailwind</h2>
+  </div>
+  <div class="but2">
+    <h2 class="technology2">Laravel</h2>
+
+  </div>
 </template>
 
 <script>
@@ -23,5 +31,33 @@ export default {
     font-family: serif;
     margin-bottom: 40%;
     margin-top: -40%;
+}
+.but1{
+    border-right: 70px solid #be61c0;
+    border-left: 70px solid #be61c0;
+    width: 30px;
+    height: 35px;
+    border-radius: 50px;
+    margin-bottom: 50%;
+    margin-top: -40%;
+    margin-left: 29%;
+}
+.but2{
+    border-right: 70px solid #be61c0;
+    border-left: 70px solid #be61c0;
+    width: 30px;
+    height: 35px;
+    border-radius: 50px;
+    margin-bottom: 50%;
+    margin-top: -643px;
+    margin-left: 17%;
+}
+.technology1{
+    color: #EFEFF0;
+    font-size: 20px;
+}
+.technology2{
+    color: #EFEFF0;
+    font-size: 20px;
 }
 </style>
