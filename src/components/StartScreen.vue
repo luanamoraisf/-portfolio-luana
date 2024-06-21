0e4ec899d6ff892917b5b46cc76d16257148bd8c
luanamoraisf <luanaflorenciopc@gmail.com>
2024-06-20 22:11:21 -0300

    adding greeting in presentation

--- src/components/StartScreen.vue
+++ src/components/StartScreen.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="data">
         <img src="/src/assets/avatar.jpg" class="avatar" />
-        <h1 id="name">Luana Morais</h1>
+        <h1 id="name">Olá, eu sou Luana Morais</h1>
         <h1 class="career">Desenvolvedora</h1>
         <h1 class="career">de Software FullStack</h1>
     </div>
@@ -27,7 +27,7 @@ export default {}
     margin-top: -2.5em;
     margin-bottom: 2em;
     font-family: serif;
-    font-size: 60px;
+    font-size: 56px;
 }
 .career{
     color: #EFEFF0;
