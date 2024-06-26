646f5706afa4d41778bc7bd24a1bb076b58283a9
luanamoraisf <luanaflorenciopc@gmail.com>
2024-06-25 21:54:37 -0300

    removing one label and fix dimensions

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
     <Disclosure as="nav" class="bg-gray-1000" style="margin-top: 1%; padding-bottom: 100%;" v-slot="{ open }">
       <div class="mx-auto max-w-7xl px-2 lg:px-6 lg:px-8">
-        <div class="relative flex h-16 justify-between ml-60">
+        <div class="relative flex h-16 justify-between ml-80">
           <div class="absolute inset-y-0 left-0 flex items-center lg:hidden">
             <!-- Mobile menu button-->
             <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -37,7 +37,6 @@
     { name: 'Início', href: '#', current: false },
     { name: 'Sobre mim', href: '#', current: false },
     { name: 'Projetos', href: '#', current: false },
-    { name: 'Experiências', href: '#', current: false },
     { name: 'Experiências', href: '#', current: false },
     { name: 'Depoimento', href: '#', current: false },
     { name: 'Contatos', href: '#', current: true },
