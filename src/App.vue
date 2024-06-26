e89772fde45b2fd96e4d99ed2dbad01d8eedf2cb
luanamoraisf <luanaflorenciopc@gmail.com>
2024-06-26 18:18:07 -0300

    style change of the scroll

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,16 @@ import Projects from './components/Projects.vue';
 body {
   background-color:#1a1d22;
 }
+body::-webkit-scrollbar {
+  width: 7px;
+}
+
+body::-webkit-scrollbar-track {
+  background: #1A1D22;
+}
+
+body::-webkit-scrollbar-thumb {
+  background-color: #be61c0;
+  border-radius: 20px;
+}
 </style>
