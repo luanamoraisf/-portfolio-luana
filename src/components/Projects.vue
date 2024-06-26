96657838840d0d8facafe8674310c80da9c90a21
luanamoraisf <luanaflorenciopc@gmail.com>
2024-06-25 21:55:36 -0300

    additing new card e fix the first

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,15 +1,36 @@
 <template>
     <h1 class="title">Projetos</h1>
-  <div class="card1"></div>
+  <a id="git1" href="https://github.com/luanamoraisf/economico" target="blank">
+    <div class="card1"></div>
 
-  <div class="but1"></div>
-  <div class="but2"></div>
+    <div class="but1"></div>
+    <div class="but2"></div>
 
-  <h2 class="technology1">Tailwind</h2>
-  <h2 class="technology2">Laravel</h2>
+    <h2 class="technology1">Tailwind</h2>
+    <h2 class="technology2">Laravel</h2>
 
-  <h2 class="project1">econômico</h2>
-  <!-- <h2>O projeto nomeado Ecônomico foi um sistema desenvolvido com o intuito de auxiliar jovens e adultos no controle financeiro.</h2> -->
+    <h2 class="project1">econômico</h2>
+    <h2 class="text1">O projeto nomeado Ecônomico foi um sistema <br> desenvolvido com o intuito de auxiliar <br> jovens e adultos no controle financeiro.</h2>
+  </a>
+
+  <a id="git2" href="https://github.com/luanamoraisf/slytherin-landing-page" target="blank">
+    <div class="card2"></div>
+
+    <div class="but3"></div>
+    <div class="but4"></div>
+    <div class="but5"></div>
+    <div class="but6"></div>
+
+    <h2 class="technology3">Bootstrap</h2>
+    <h2 class="technology4">HTML/CSS</h2>
+    <h2 class="technology5">jQuery</h2>
+
+
+    <h2 class="project2"></h2>
+    <h2 class="text2"></h2>
+  </a>
+  
+  <img src="">
 </template>
 
 <script>
@@ -26,6 +47,7 @@ export default {
     border-right: 250px solid #EFEFF0;
     border-left: 250px solid #EFEFF0;
     margin-top: -32%;
+    margin-left: 1%;
 }
 .title{
     color: #EFEFF0;
@@ -58,7 +80,7 @@ export default {
     font-size: 20px;
     margin-bottom: 50%;
     margin-top: -52.7%;
-    margin-right: 30.5%;
+    margin-right: 30%;
 }
 .technology2{
     color: #EFEFF0;
@@ -72,7 +94,77 @@ export default {
     font-size: 30px;
     margin-bottom: 50%;
     margin-top: -48%;
-    margin-right: 82%;
+    margin-right: 81%;
     font-weight: bold;
+}
+.text1{
+    color: #1A1D22;
+    font-size: 19px;
+    margin-bottom: 50%;
+    margin-top: -49%;
+    margin-right: 58%;
+}
+#git1{
+    cursor: pointer;
+}
+.card2{
+    width: 400px;
+    height: 500px;
+    border-radius: 5%;
+    border-right: 250px solid #EFEFF0;
+    border-left: 250px solid #EFEFF0;
+    margin-left: 58%;
+    margin-top: -67.5%;
+}
+.but3{
+    border-right: 70px solid #be61c0;
+    border-left: 70px solid #be61c0;
+    width: 30px;
+    height: 35px;
+    border-radius: 50px;
+    margin-bottom: 50%;
+    margin-top: -40%;
+    margin-left: 85.5%;
+}
+.but4{
+    border-right: 70px solid #be61c0;
+    border-left: 70px solid #be61c0;
+    width: 30px;
+    height: 35px;
+    border-radius: 50px;
+    margin-bottom: 50%;
+    margin-top: -52.9%;
+    margin-left: 73%;
+}
+.but5{
+    border-right: 70px solid #be61c0;
+    border-left: 70px solid #be61c0;
+    width: 30px;
+    height: 35px;
+    border-radius: 50px;
+    margin-bottom: 50%;
+    margin-top: -52.9%;
+    margin-left: 60.3%;
+}
+.technology3{
+    color: #EFEFF0;
+    font-size: 20px;
+    margin-bottom: 50%;
+    margin-top: -52.8%;
+    margin-left: 32.2%;
+}
+.technology4{
+    color: #EFEFF0;
+    font-size: 20px;
+    margin-bottom: 50%;
+    margin-top: -52.4%;
+    margin-left: 57.5%;
+}
+.technology5{
+    color: #EFEFF0;
+    font-size: 20px;
+    margin-bottom: 50%;
+    margin-top: -52.5%;
+    margin-left: 82.6%;
 }
 </style>
