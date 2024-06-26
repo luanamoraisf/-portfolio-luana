310aa544cbd0db82865b7c81bc8bee90d5a449a0
luanamoraisf <luanaflorenciopc@gmail.com>
2024-06-26 18:18:49 -0300

    implementing project cards

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,6 +1,6 @@
 <template>
     <h1 class="title">Projetos</h1>
-  <a id="git1" href="https://github.com/luanamoraisf/economico" target="blank">
+  <a class="git" href="https://github.com/luanamoraisf/economico" target="blank">
     <div class="card1"></div>
 
     <div class="but1"></div>
@@ -11,26 +11,69 @@
 
     <h2 class="project1">econômico</h2>
     <h2 class="text1">O projeto nomeado Ecônomico foi um sistema <br> desenvolvido com o intuito de auxiliar <br> jovens e adultos no controle financeiro.</h2>
+
+    <img src="">
   </a>
 
-  <a id="git2" href="https://github.com/luanamoraisf/slytherin-landing-page" target="blank">
+  <a class="git" href="https://github.com/luanamoraisf/slytherin-landing-page" target="blank">
     <div class="card2"></div>
 
     <div class="but3"></div>
     <div class="but4"></div>
     <div class="but5"></div>
-    <div class="but6"></div>
 
     <h2 class="technology3">Bootstrap</h2>
     <h2 class="technology4">HTML/CSS</h2>
     <h2 class="technology5">jQuery</h2>
 
 
-    <h2 class="project2"></h2>
-    <h2 class="text2"></h2>
+    <h2 class="project2">Slytherin landing page</h2>
+    <h2 class="text2">Projeto acadêmico de FrontEnd, landing page <br> dedicada a casa Slytherin da saga Harry Potter.</h2>
+
+    <img src="">
+  </a>
+
+  <a class="git" href="https://github.com/luanamoraisf/node_api" target="blank">
+    <div class="card3"></div>
+
+    <div class="but6"></div>
+
+    <h2 class="technology6">Node.js</h2>
+
+
+    <h2 class="project3">API Node</h2>
+    <h2 class="text3">Projeto de uma API simples em Node, <br> com retorno de dados de usuários.</h2>
+
+    <img src="">
+  </a>
+
+  <a class="git" href="https://github.com/luanamoraisf/provocare" target="blank">
+    <div class="card4"></div>
+
+    <div class="but7"></div>
+
+    <h2 class="technology7">Vue.js</h2>
+
+
+    <h2 class="project4">Provocare</h2>
+    <h2 class="text4">Projeto acadêmcio de um sistema para criar <br> desafios ou/e participar para tentar solucionar, <br> na área de programação.</h2>
+
+    <img src="">
   </a>
   
-  <img src="">
+  <a class="git" href="https://github.com/luanamoraisf/data-collect" target="blank">
+    <div class="card5"></div>
+
+    <div class="but8"></div>
+
+    <h2 class="technology8">Python</h2>
+
+
+    <h2 class="project5">Web Scraping</h2>
+    <h2 class="text5">Um web scraping que coleta dados de uma rede <br> social sobre livros, para um projeto acadêmico de <br> pesquisa com o objetivo da criação de um <br> corpus de opiniões sobre livros.</h2>
+
+    <img src="">
+  </a>
 </template>
 
 <script>
@@ -63,7 +106,7 @@ export default {
     border-radius: 50px;
     margin-bottom: 50%;
     margin-top: -40%;
-    margin-left: 29%;
+    margin-left: 28%;
 }
 .but2{
     border-right: 70px solid #be61c0;
@@ -73,21 +116,21 @@ export default {
     border-radius: 50px;
     margin-bottom: 50%;
     margin-top: -643px;
-    margin-left: 17%;
+    margin-left: 15.4%;
 }
 .technology1{
     color: #EFEFF0;
     font-size: 20px;
     margin-bottom: 50%;
     margin-top: -52.7%;
-    margin-right: 30%;
+    margin-right: 32.5%;
 }
 .technology2{
     color: #EFEFF0;
     font-size: 20px;
     margin-bottom: 50%;
     margin-top: -52.5%;
-    margin-right: 54%;
+    margin-right: 57.5%;
 }
 .project1{
     color: #1A1D22;
@@ -104,7 +147,7 @@ export default {
     margin-top: -49%;
     margin-right: 58%;
 }
-#git1{
+.git{
     cursor: pointer;
 }
 .card2{
@@ -166,5 +209,144 @@ export default {
     margin-bottom: 50%;
     margin-top: -52.5%;
     margin-left: 82.6%;
+}
+.project2{
+    color: #1A1D22;
+    font-size: 30px;
+    font-weight: bold;
+    margin-bottom: 50%;
+    margin-top: -48%;
+    margin-left: 48%; 
+}
+.text2{
+    color: #1A1D22;
+    font-size: 19px;
+    margin-bottom: 50%;
+    margin-top: -49%;
+    margin-left: 56%;
+}
+.card3{
+    width: 400px;
+    height: 500px;
+    border-radius: 5%;
+    border-right: 250px solid #EFEFF0;
+    border-left: 250px solid #EFEFF0;
+    margin-top: -14%;
+    margin-left: 1%;
+}
+.but6{
+    border-right: 70px solid #be61c0;
+    border-left: 70px solid #be61c0;
+    width: 30px;
+    height: 35px;
+    border-radius: 50px;
+    margin-bottom: 50%;
+    margin-top: -40%;
+    margin-left: 28%;
+}
+.technology6{
+    color: #EFEFF0;
+    font-size: 20px;
+    margin-bottom: 50%;
+    margin-top: -52.7%;
+    margin-right: 32.5%;
+}
+.project3{
+    color: #1A1D22;
+    font-size: 30px;
+    margin-bottom: 50%;
+    margin-top: -48%;
+    margin-right: 82%;
+    font-weight: bold;
+}
+.text3{
+    color: #1A1D22;
+    font-size: 19px;
+    margin-bottom: 50%;
+    margin-top: -49%;
+    margin-right: 58%;
+}
+.card4{
+    width: 400px;
+    height: 500px;
+    border-radius: 5%;
+    border-right: 250px solid #EFEFF0;
+    border-left: 250px solid #EFEFF0;
+    margin-left: 58%;
+    margin-top: -65.3%;
+}
+.but7{
+    border-right: 70px solid #be61c0;
+    border-left: 70px solid #be61c0;
+    width: 30px;
+    height: 35px;
+    border-radius: 50px;
+    margin-bottom: 50%;
+    margin-top: -40%;
+    margin-left: 85.5%;
+}
+.technology7{
+    color: #EFEFF0;
+    font-size: 20px;
+    margin-bottom: 50%;
+    margin-top: -52.8%;
+    margin-left: 82.2%;
+}
+.project4{
+    color: #1A1D22;
+    font-size: 30px;
+    font-weight: bold;
+    margin-bottom: 50%;
+    margin-top: -48%;
+    margin-left: 33%; 
+}
+.text4{
+    color: #1A1D22;
+    font-size: 19px;
+    margin-bottom: 50%;
+    margin-top: -49%;
+    margin-left: 56%;
+}
+.card5{
+    width: 400px;
+    height: 500px;
+    border-radius: 5%;
+    border-right: 250px solid #EFEFF0;
+    border-left: 250px solid #EFEFF0;
+    margin-bottom: 50%;
+    margin-left: 29%;
+    margin-top: -16%;
+}
+.but8{
+    border-right: 70px solid #be61c0;
+    border-left: 70px solid #be61c0;
+    width: 30px;
+    height: 35px;
+    border-radius: 50px;
+    margin-bottom: 50%;
+    margin-top: -89.8%;
+    margin-left: 56.4%;
+}
+.technology8{
+    color: #EFEFF0;
+    font-size: 20px;
+    margin-bottom: 50%;
+    margin-top: -52.8%;
+    margin-left: 24.5%;
+}
+.project5{
+    color: #1A1D22;
+    font-size: 30px;
+    font-weight: bold;
+    margin-bottom: 50%;
+    margin-top: -48%;
+    margin-right: 19%; 
+}
+.text5{
+    color: #1A1D22;
+    font-size: 19px;
+    margin-bottom: 50%;
+    margin-top: -49%;
+    margin-right: 1.5%;
 }
 </style>
