e2a532db2d6f098d5fc0667ddcb9beea2ccb18db
luanamoarisf <luanaflorenciopc@gmail.com>
2024-07-10 17:56:20 -0300

    adding language labels

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,17 +1,51 @@
 <template>
   <h1 id="title">Experiências</h1>
 
-  <div>
-    <img id="laravel" src="../assets/experiences/Laravel-Logo.wine.webp">
-    <img id="js" src="../assets/experiences/javascript-logo.webp">
-    <img id="ror" src="../assets/experiences/Ruby_on_Rails-Logo.wine.webp">    
-    <img id="php" src="../assets/experiences/PHP-Logo.webp">    
-    <img id="vue" src="../assets/experiences/logovue.webp">  
-    <img id="sql" src="../assets/experiences/MySQL-Logo.wine.webp">
-    <img id="html" src="../assets/experiences/html5-logo.webp">
-    <img id="css" src="../assets/experiences/CSS-Logo-500x313.webp">
-    <img id="boots" src="../assets/experiences/Bootstrap-logo.webp">      
+  <div class="linguages">
+    <a href="https://laravel.com/">
+      <img id="laravel" src="../assets/experiences/Laravel-Logo.wine.webp">
+    </a>
+    <h2 class="lgs" id="laravel-label">Laravel</h2>
 
+    <a href="https://www.javascript.com/">
+      <img id="js" src="../assets/experiences/javascript-logo.webp">
+    </a>
+    <h2 class="lgs" id="js-label">Javascript</h2>
+
+    <a href="https://rubyonrails.org/">
+      <img id="ror" src="../assets/experiences/Ruby_on_Rails-Logo.wine.webp">    
+    </a>
+    <h2 class="lgs" id="ror-label">Ruby on Rails</h2>
+
+    <a href="https://www.php.net/">
+      <img id="php" src="../assets/experiences/PHP-Logo.webp">    
+    </a>
+    <h2 class="lgs" id="php-label">PHP</h2>
+
+    <a href="https://vuejs.org/">
+      <img id="vue" src="../assets/experiences/logovue.webp">  
+    </a>
+    <h2 class="lgs" id="vue-label">Vue.js</h2>
+
+    <a href="https://www.mysql.com/">
+      <img id="sql" src="../assets/experiences/MySQL-Logo.wine.webp">
+    </a>
+    <h2 class="lgs" id="sql-label">MySQL</h2>
+
+    <a href="">
+      <img id="html" src="../assets/experiences/html5-logo.webp">
+    </a>
+    <h2 class="lgs" id="html-label">HTML</h2>
+
+    <a href="">
+      <img id="css" src="../assets/experiences/CSS-Logo-500x313.webp">
+    </a>
+    <h2 class="lgs" id="css-label">CSS</h2>
+
+    <a href="https://getbootstrap.com/">
+      <img id="boots" src="../assets/experiences/Bootstrap_logo.svg">      
+    </a>
+    <h2 class="lgs" id="boots-label">Bootstrap</h2>
   </div>
   
 </template>
@@ -29,55 +63,100 @@ export default {
   margin-top: 15%;
   margin-bottom: 5%;
 }
+.linguages{
+  cursor: pointer;
+}
 #laravel{
-  width: 350px;
-  height: 280px;
+  width: 360px;
+  height: 290px;
   margin-left: 2%;
 }
 #js{
   width: 200px;
   height: 200px;
   margin-left: 40%;
-  margin-top: -19.5%
+  margin-top: -33%
 }
 #ror{
   width: 350px;
   height: 280px;
   margin-left: 68%;
-  margin-top: -20%
+  margin-top: -33%
 }
 #php{
-  margin-left: 3%;
-  margin-top: 10%;
+  margin-left: 2%;
 }
 #vue{
   width: 220px;
   height: 180px;
   margin-left: 39%;
-  margin-top: -15%;
+  margin-top: -19%;
+  margin-bottom: 1%;
 }
 #sql{
   width: 340px;
   height: 300px;
   margin-left: 69%;
-  margin-top: -20%;
+  margin-top: -24%;
 }
 #html{
   width: 200px;
   height: 250px;
   margin-left: 5%;
-  margin-top: 10%;
+  margin-top: 12%;
 }
 #css{
   width: 450px;
   height: 260px;
   margin-left: 30%;
-  margin-top: -21%
+  margin-top: -25.5%
 }
 #boots{
-  width: 400px;
-  height: 300px;
-  margin-left: 66.5%;
-  margin-top: -21%
+  width: 260px;
+  height: 220px;
+  margin-left: 73%;
+  margin-top: -23%
+}
+.lgs{
+  color: #EFEFF0;
+  font-size: 22px;
+}
+#laravel-label{
+  margin-right: 70%;
+  margin-bottom: 12%;
+  margin-top: -2%;
+}
+#js-label{
+  margin-right: 4%;
+  margin-top: 2%;
+  margin-bottom: 10%;
+}
+#ror-label{
+  margin-left: 67%;
+  margin-top: -2.5%;
+  margin-bottom: 10%;
+}
+#php-label{
+  margin-right: 71%;
+  margin-top: 2%;
+}
+#vue-label{
+  margin-right: 4%;
+}
+#sql-label{
+  margin-left: 67%;
+  margin-top: -3%;
+}
+#html-label{
+  margin-right: 74%;
+  margin-top: 2%;
+}
+#css-label{
+  margin-right: 3.3%;
+  margin-top: 2%;
+}
+#boots-label{
+  margin-left: 68%;
+  margin-top: 2%;
 }
 </style>
