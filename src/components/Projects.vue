998ebce14a4d68f91d69a6ddf9c0e5988bd26db2
luanamoarisf <luanaflorenciopc@gmail.com>
2024-07-11 21:50:19 -0300

    adding subtitle in projects screen

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,5 +1,6 @@
 <template>
     <h1 class="title">Projetos</h1>
+    <h2 class="subtitle">Clique nos cards para ver o código de cada projeto em seus repositórios.</h2>
 
     <div class="git">
         <Card1/>
@@ -27,5 +28,11 @@ import Card5 from './projects/Card5.vue';
 }
 .git{
     cursor: pointer;
+}
+.subtitle{
+    color: #EFEFF0;
+    font-size: 22px;
+    margin-bottom: 39%;
+    margin-top: -39%;
 }
 </style>
