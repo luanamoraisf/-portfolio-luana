0450971c81586b02ed27f17ff0e61a5e2028ab62
luanamoarisf <luanaflorenciopc@gmail.com>
2024-07-15 20:54:21 -0300

    finishing footer

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -3,14 +3,14 @@
 
     <div id="icons">
         <a href="https://github.com/luanamoraisf" target="_blank">
-            <img id="git" src="../assets/contact/icons8-github-50.png">
+            <img id="git" src="../assets/contact/icons8-github-64.png">
         </a>
         <a href="https://www.linkedin.com/in/luana-florencio/" target="_blank">
-            <img id="linkedin" src="../assets/contact/icons8-linkedin-50.png">
+            <img id="linkedin" src="../assets/contact/icons8-linkedin-64.png">
         </a>
-        <img src="../assets/contact/icons8-telefone-celular-64.png">
+        <img id="cell" src="../assets/contact/icons8-telefone-celular-64.png">
         <h2 class="datas" id="number">(81)9 8676-5325</h2>
-        <img id="email" src="../assets/contact/icons8-nova-mensagem-50.png">
+        <img id="email" src="../assets/contact/icons8-enviar-60.png">
         <h2 class="datas" id="mail">luanaflorenciopc@gmail.com</h2>
 
     </div>
@@ -25,18 +25,19 @@
 }
 #icons{
     margin-top: 4%;
+    margin-bottom: 3%;
 }
 #email{
     margin-left: 0.5%;
     margin-top: 4%;
+
 }
 #git{
-    margin-left: 0.5%;
-    /* margin-top: 3.5%; */
+    margin-left: 40%;
     margin-bottom: 3%;
 }
 #linkedin{
-    margin-left: 0.5%;
+    margin-left: 40%;
     margin-bottom: 3%;
 
 }
@@ -47,9 +48,14 @@
 #number{
     margin-right: 72%;
     margin-top: -3.5%;
+    /* margin-left: 20%; */
 }
 #mail{
     margin-right: 62%;
     margin-top: -3%;
+}
+#cell{
+    margin-left: 0.4%;
+    margin-top: -16.6%;
 }
 </style>
