6fee2c7b061eb64ccf21458c5cb94c99e4ac98d4
luanamoarisf <luanaflorenciopc@gmail.com>
2024-09-09 18:38:07 -0300

    implements tailwind class for screens resposives

--- src/components/StartScreen.vue
+++ src/components/StartScreen.vue
@@ -1,10 +1,10 @@
 <template>
     <section id="start-screen">
         <div class="data">
-            <img src="/src/assets/avatar.jpg" class="avatar" />
-            <h1 id="name">Olá, eu sou Luana Morais</h1>
-            <h1 class="career">Desenvolvedora</h1>
-            <h1 class="career">de Software FullStack</h1>
+            <img src="/src/assets/avatar.jpg" class="w-64 rounded-full border-4 border-[#be61c0]" />
+            <h1 id="name" class="text-[#EFEFF0] mb-32 text-base font-serif">Olá, eu sou Luana Morais</h1>
+            <h1 class="text-[#EFEFF0] text-xl font-serif">Desenvolvedora</h1>
+            <h1 class="text-[#EFEFF0] text-xl font-serif">de Software FullStack</h1>
         </div>
     </section>
 </template>
@@ -18,26 +18,10 @@ export default {}
     padding-top: 109%;
 }
 .data{
-    margin-top: -70em;
-}
-.avatar {
-    width: 270px;
-    height: 270px;
-    border-radius: 100%;
-    border: 5px solid #be61c0;
-    position: relative;
+    margin-top: -71em;
 }
 #name {
-    color: #EFEFF0;
     margin-top: -2.5em;
-    margin-bottom: 2.5em;
-    font-family: serif;
-    font-size: 56px;
-}
-.career{
-    color: #EFEFF0;
-    font-size: 66px;
-    font-family: serif;
 }
 </style>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    fontSize: {
+      sm: '2rem',
+      base: '3.3rem',
+      xl: '4.2rem',
+      '2xl': '4.5rem',
+    },
     extend: {}
   },
   plugins: []
