53d69136c90e6ce0612da282ba4388a45ce7f21f
luanamoarisf <luanaflorenciopc@gmail.com>
2024-09-10 17:10:15 -0300

    finalizes responsiveness of first screen

--- src/components/StartScreen.vue
+++ src/components/StartScreen.vue
@@ -1,10 +1,10 @@
 <template>
     <section id="start-screen">
         <div class="data">
-            <img src="/src/assets/avatar.jpg" class="sm:w-32 md:w-40 lg:w-64 xl:w-64 rounded-full border-4 border-[#be61c0]" />
-            <h1 id="name" class="text-[#EFEFF0] mb-32 text-base font-serif">Olá, eu sou Luana Morais</h1>
-            <h1 class="text-[#EFEFF0] text-xl font-serif">Desenvolvedora</h1>
-            <h1 class="text-[#EFEFF0] text-xl font-serif">de Software FullStack</h1>
+            <img src="/src/assets/avatar.jpg" class="sm:w-36 md:w-48 sm:ml-60 md:ml-28 lg:ml-0 lg:w-64 xl:w-64 rounded-full border-4 border-[#be61c0]" />
+            <h1 id="name" class="text-[#EFEFF0] mb-32 sm:text-sm md:text-md lg:text-base font-serif">Olá, eu sou Luana Morais</h1>
+            <h1 class="text-[#EFEFF0] lg:text-xl md:text-base sm:text-md  font-serif">Desenvolvedora</h1>
+            <h1 class="text-[#EFEFF0] lg:text-xl md:text-base sm:text-md font-serif">de Software FullStack</h1>
         </div>
     </section>
 </template>
