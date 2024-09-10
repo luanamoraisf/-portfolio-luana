1b87cd56afa76061d5e3b875ae87aedb2fa2d60c
luanamoarisf <luanaflorenciopc@gmail.com>
2024-09-09 22:48:18 -0300

    applies screen responsiveness to the avatar element

--- src/components/StartScreen.vue
+++ src/components/StartScreen.vue
@@ -1,7 +1,7 @@
 <template>
     <section id="start-screen">
         <div class="data">
-            <img src="/src/assets/avatar.jpg" class="w-64 rounded-full border-4 border-[#be61c0]" />
+            <img src="/src/assets/avatar.jpg" class="sm:w-32 md:w-40 lg:w-64 xl:w-64 rounded-full border-4 border-[#be61c0]" />
             <h1 id="name" class="text-[#EFEFF0] mb-32 text-base font-serif">Olá, eu sou Luana Morais</h1>
             <h1 class="text-[#EFEFF0] text-xl font-serif">Desenvolvedora</h1>
             <h1 class="text-[#EFEFF0] text-xl font-serif">de Software FullStack</h1>
