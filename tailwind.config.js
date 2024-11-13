114d4ecea05440be7b64c7e247a9e6654b606c5b
luanamoarisf <luanaflorenciopc@gmail.com>
2024-11-13 17:44:06 -0300

    adding size xxsm

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     fontSize: {
+      xxsm: '1.3rem',
       xsm: '1.5rem',
       sm: '2rem',
       md: '2.8rem',
