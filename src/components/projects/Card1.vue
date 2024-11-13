b786cbaf3e4dea29a7b707fd40859ce9f3a62608
luanamoarisf <luanaflorenciopc@gmail.com>
2024-11-13 17:45:30 -0300

    implementing tailwind class

--- src/components/projects/Card1.vue
+++ src/components/projects/Card1.vue
@@ -3,16 +3,16 @@
         <div id="card1" class="w-96 rounded-3xl ml-3 cursor-pointer"></div>
 
         <div id="label1" class="w-7 h-9 rounded-full"></div>
-        <div id="label2" class="w-7 h-9 rounded-full"></div>
+        <div id="label2" class="w-7 h-9 rounded-full ml-9"></div>
         <div id="label3" class="w-7 h-9 rounded-full"></div>
 
 
-        <h2 class="technology1">Tailwind</h2>
-        <h2 class="technology2">Laravel</h2>
-        <h2 class="technology3">MySQL</h2>
+        <h2 id="technology1" class="text-[#EFEFF0] text-xxsm">Tailwind</h2>
+        <h2 id="technology2" class="text-[#EFEFF0] text-xxsm">Laravel</h2>
+        <h2 id="technology3" class="text-[#EFEFF0] text-xxsm">MySQL</h2>
 
-        <h2 class="project1">Econômico</h2>
-        <h2 class="text1">Aplicação com sistema de autenticação <br> desenvolvido com o intuito de auxiliar <br> jovens e adultos no controle financeiro.</h2>
+        <h2 id="project1" class="text-[#1A1D22] text-sm font-bold">Econômico</h2>
+        <h2 id="text1" class="text-[#1A1D22] text-xxsm">Aplicação com sistema de autenticação <br> desenvolvido com o intuito de auxiliar <br> jovens e adultos no controle financeiro.</h2>
 
         <div class="picture">
             <img id="picture1" src="../../assets/projects/economico2.png">
@@ -56,7 +56,6 @@ export default {
     border-left: 70px solid #be61c0;
     margin-bottom: 50%;
     margin-top: -643px;
-    margin-left: 2.8%;
 }
 #label3{
     border-right: 70px solid #be61c0;
@@ -65,38 +64,27 @@ export default {
     margin-top: -643px;
     margin-left: 15.4%;
 }
-.technology1{
-    color: #EFEFF0;
-    font-size: 20px;
+#technology1{
     margin-bottom: 50%;
-    margin-top: -52.7%;
+    margin-top: -52.9%;
     margin-right: 32.5%;
 }
-.technology2{
-    color: #EFEFF0;
-    font-size: 20px;
+#technology2{
     margin-bottom: 50%;
-    margin-top: -52.5%;
+    margin-top: -52.6%;
     margin-right: 82.6%;
 }
-.technology3{
-    color: #EFEFF0;
-    font-size: 20px;
+#technology3{
     margin-bottom: 50%;
-    margin-top: -52.5%;
+    margin-top: -52.6%;
     margin-right: 57.5%;
 }
-.project1{
-    color: #1A1D22;
-    font-size: 30px;
+#project1{
     margin-bottom: 50%;
     margin-top: -48%;
     margin-right: 81%;
-    font-weight: bold;
 }
-.text1{
-    color: #1A1D22;
-    font-size: 19px;
+#text1{
     margin-bottom: 50%;
     margin-top: -49%;
     margin-right: 58%;
