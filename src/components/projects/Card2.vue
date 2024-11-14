2e1eae818e1de85a7513b6ddc921dfad908823fd
luanamoarisf <luanaflorenciopc@gmail.com>
2024-11-13 23:17:20 -0300

    implementing tailwind class in card2

--- src/components/projects/Card2.vue
+++ src/components/projects/Card2.vue
@@ -1,18 +1,18 @@
 <template>
     <a href="https://github.com/luanamoraisf/slytherin-landing-page" target="blank">
-        <div id="card2" style="cursor: pointer;"></div>
+        <div id="card2" class="w-96 rounded-3xl cursor-pointer"></div>
 
-        <div class="but4"></div>
-        <div class="but5"></div>
-        <div class="but6"></div>
+        <div id="but4" class="w-8 h-9 rounded-full"></div>
+        <div id="but5" class="w-8 h-9 rounded-full"></div>
+        <div id="but6" class="w-8 h-9 rounded-full"></div>
 
-        <h2 class="technology4">Bootstrap</h2>
-        <h2 class="technology5">HTML/CSS</h2>
-        <h2 class="technology6">jQuery</h2>
+        <h2 id="technology4" class="text-[#EFEFF0] text-xxsm">Bootstrap</h2>
+        <h2 id="technology5" class="text-[#EFEFF0] text-xxsm">HTML/CSS</h2>
+        <h2 id="technology6" class="text-[#EFEFF0] text-xxsm">jQuery</h2>
 
 
-        <h2 class="project2">Slytherin landing page</h2>
-        <h2 class="text2">Projeto acadêmico FrontEnd, landing page com <br> funcionalidades na NavBar, footer e <br> formulário com envio para entrar em contato.</h2>
+        <h2 id="project2" class="text-[#1A1D22] text-sm font-bold">Slytherin landing page</h2>
+        <h2 id="text2" class="text-[#1A1D22] text-min">Projeto acadêmico FrontEnd, landing page com <br> funcionalidades na NavBar, footer e <br> formulário com envio para entrar em contato.</h2>
 
         <div class="picture">
             <img id="picture2" src="../../assets/projects/slytherin.png">
@@ -28,76 +28,54 @@ export default {
 
 <style>
 #card2{
-    width: 400px;
     height: 500px;
-    border-radius: 5%;
     border-right: 250px solid #EFEFF0;
     border-left: 250px solid #EFEFF0;
     margin-left: 58%;
     margin-top: -41.2%;
 }
-.but4{
+#but4{
     border-right: 70px solid #be61c0;
     border-left: 70px solid #be61c0;
-    width: 30px;
-    height: 35px;
-    border-radius: 50px;
     margin-bottom: 50%;
     margin-top: -40%;
     margin-left: 85.5%;
 }
-.but5{
+#but5{
     border-right: 70px solid #be61c0;
     border-left: 70px solid #be61c0;
-    width: 30px;
-    height: 35px;
-    border-radius: 50px;
     margin-bottom: 50%;
     margin-top: -52.9%;
     margin-left: 73%;
 }
-.but6{
+#but6{
     border-right: 70px solid #be61c0;
     border-left: 70px solid #be61c0;
-    width: 30px;
-    height: 35px;
-    border-radius: 50px;
     margin-bottom: 50%;
     margin-top: -52.9%;
     margin-left: 60.3%;
 }
-.technology4{
-    color: #EFEFF0;
-    font-size: 20px;
+#technology4{
     margin-bottom: 50%;
     margin-top: -52.8%;
     margin-left: 32.2%;
 }
-.technology5{
-    color: #EFEFF0;
-    font-size: 20px;
+#technology5{
     margin-bottom: 50%;
-    margin-top: -52.4%;
+    margin-top: -52.6%;
     margin-left: 57.5%;
 }
-.technology6{
-    color: #EFEFF0;
-    font-size: 20px;
+#technology6{
     margin-bottom: 50%;
-    margin-top: -52.5%;
+    margin-top: -52.7%;
     margin-left: 82.6%;
 }
-.project2{
-    color: #1A1D22;
-    font-size: 30px;
-    font-weight: bold;
+#project2{
     margin-bottom: 50%;
     margin-top: -48%;
     margin-left: 48%; 
 }
-.text2{
-    color: #1A1D22;
-    font-size: 19px;
+#text2{
     margin-bottom: 50%;
     margin-top: -49%;
     margin-left: 56%;
